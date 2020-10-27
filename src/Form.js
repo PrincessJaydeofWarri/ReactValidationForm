8c71ee5632b0649392bed44cf4a26eed6b6a7815
Jayde Pearson <etana@Astute.local>
2020-10-27 12:31:55 +0000

    update colors

--- src/Form.js
+++ src/Form.js
@@ -3,8 +3,9 @@ import FormSignup from './FormSignup'
 import FormSuccess from './FormSuccess'
 import './Form.css'
 
+
 const Form =() => {
-  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [isSubmitted, setIsSubmitted,] = useState(false)
 
   function submitForm() {
     setIsSubmitted(true);
